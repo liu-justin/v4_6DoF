e1c546290d0afa3051f79940f297c5831358c7b2
Liu <justin.liu@thermofisher.com>
2020-10-01 17:18:21 -0500

    FK is working finally, needed to inverse the Blist; IK needs timestep to get proper guesses, so now I am going to SOLIDWORKS to get mass properties for the links

--- first_try.py
+++ first_try.py
@@ -14,7 +14,6 @@ rpy_ee = [float(n) for n in obj.robot.joint[len(obj.robot.joint)-1].origin["rpy"
 R_ee = mr.RollPitchYawToRot(rpy_ee[0],rpy_ee[1],rpy_ee[2])
 p_ee = [float(n) for n in obj.robot.joint[len(obj.robot.joint)-1].origin["xyz"].split()]
 T_ee = mr.RpToTrans(R_ee, p_ee)
-print (T_ee)
 
 joint_list = [joint for joint in obj.robot.joint if joint["type"]!="fixed"]
 
@@ -26,7 +25,7 @@ for joint in reversed(joint_list): # need to skip type fixed
 
     # this T takes previous joint to current joint, or is current joint relative to prev joint
     # T_56, T_lower_higher
-    T = np.round(mr.RpToTrans(R,p),10)
+    T = mr.RpToTrans(R,p)
     TList.insert(0,T)
     # print(np.round(T,5))
 
@@ -37,15 +36,28 @@ for joint in reversed(joint_list): # need to skip type fixed
     current_v = -1*np.dot(current_omega_skewed, p_ee)
 
     body_axis = np.r_[current_omega, current_v]
-    print(body_axis)
+    # print(body_axis)
     bodyList.insert(0,body_axis)
 
-    T_ee = np.round(np.dot(T, T_ee),5)
-    # print(T_ee)
-    
-
+    T_ee = np.dot(T, T_ee)
 
 # alright, DH parameters use frames, and PoE only needs home and EE frames
 # however, URDF has all frames b/c it needs them for mass crap in ch 8
 # for FK, can just use home frame, ee frame, and calculate the body axes from the frames
-# mr.FKinBody()
+angleList = np.array([0,-1*np.pi/2,np.pi/2,0,-1*np.pi/2,0])
+bodyList = np.array(bodyList).T
+
+final = mr.FKinBody(T_ee, bodyList, angleList)
+print(np.round(T_ee, 5))
+print(np.round(final,5))
+
+M = np.array([[-1, 0,  0, 0],
+                      [ 0, 1,  0, 6],
+                      [ 0, 0, -1, 2],
+                      [ 0, 0,  0, 1]])
+Blist = np.array([[0, 0, -1, 2, 0,   0],
+                          [0, 0,  0, 0, 1,   0],
+                          [0, 0,  1, 0, 0, 0.1]]).T
+thetalist = np.array([np.pi / 2.0, 3, np.pi])
+
+# print(np.round(mr.FKinBody(M,Blist, thetalist),5))
